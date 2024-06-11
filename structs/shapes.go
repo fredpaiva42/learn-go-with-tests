13bbed81c0c2c45099819142247fc8283082d0df
Frederico Gomes de Paiva <frederico.paiva@ifood.com.br>
2024-06-11 17:23:21 -0300

    feat: add function to caulculate Circle area, and methods

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,30 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Perimeter returns the perimeter of a rectangle.
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area returns the area of a rectangle.
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
